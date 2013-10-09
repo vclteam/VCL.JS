d4cda629124dd235793f9f6b784aa4ad54a5f338
NatiKr <nati.krisi@gmail.com>
2013-10-09 06:30:58 +0300

    'update'

--- VCL/VXConst.ts
+++
@@ -1,4 +0,0 @@
-export class VXConst {
-    public static _LOGINFAILD: string = "Login faild,Incorrect Username or Password!";
-    public static _ERRONLYCONTAINER: string = "only container components can own components"
-}
